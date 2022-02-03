b5d5d1e42ee2ffe60a8de46b20d8df840e8d8ee3
menshikov <menshchikov.aleksandr@ya.ru>
2022-02-03 23:02:57 +0700

    project

---
+++ assets/src/js/my.js
@@ -0,0 +1,11 @@
+// body loaded
+// $(window).on('load', function () {
+//     $body  = $('body');
+//     setTimeout (function(){
+//         $body.addClass('loaded');
+//     });
+// });
+
+// $(document).ready(function(){
+//
+// });
